19beab1507465effd394789b6c775996267c7082
Mika <mika.sarajarvi@gmail.com>
2023-11-17 14:35:44 +0200

    taustakuva

--- app/src/main/java/com/example/karttasovellus/HomeScreen.kt
+++ app/src/main/java/com/example/karttasovellus/HomeScreen.kt
@@ -1,13 +1,29 @@
 package com.example.karttasovellus
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
+import com.example.karttasovellus.R
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
-    Button(onClick = { navController.navigate("map") }) {
-        Text("Avaa kartta")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.map_background),
+            contentDescription = "Taustakuva",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop // Skaalaa kuva sopivaksi
+        )
+
+        Button(onClick = { navController.navigate("map") }) {
+            Text("Avaa kartta")
+        }
     }
 }
